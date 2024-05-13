9c06051633cdec750c7afd6416aa2fdb793976db
Rav Sispal <rav.sispal@ravsispal.com>
2024-05-13 02:39:47 +0100

    Add Style Components CSS registry wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 "use client";
+import StyledComponentsRegistry from "@/lib/registry";
 import * as GovUK from "govuk-react";
 import { Link, SearchBox } from "govuk-react";
 
@@ -9,38 +10,40 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <GovUK.GlobalStyle />
-      <body>
-        <GovUK.TopNav
-          bgColor="#000033"
-          serviceTitle={
-            <GovUK.TopNav.Anchor as={Link} to="/">
-              CMS Renderer Frontend
-            </GovUK.TopNav.Anchor>
-          }
-          search={
-            <GovUK.SearchBox>
-              {SearchBox.Input && <SearchBox.Input placeholder="Search GOV.UK" />}
-              {SearchBox.Button && <SearchBox.Button />}
-            </GovUK.SearchBox>
-          }>
-          <GovUK.TopNav.NavLink as={Link} to="/">
-            Home
-          </GovUK.TopNav.NavLink>
-          <GovUK.TopNav.NavLink as={Link} to="/forms">
-            Forms
-          </GovUK.TopNav.NavLink>
-        </GovUK.TopNav>
-        <GovUK.Page.WidthContainer>
-          <GovUK.PhaseBanner level="alpha">
-            This part of GOV.UK is being rebuilt – <Link href="https://example.com">find out what that means</Link>
-          </GovUK.PhaseBanner>
-          <GovUK.Page.Main>{children}</GovUK.Page.Main>
-        </GovUK.Page.WidthContainer>
-        <div style={{ position: "fixed", bottom: 0, width: "100vw" }}>
-          <GovUK.Footer />
-        </div>
-      </body>
+      <StyledComponentsRegistry>
+        <GovUK.GlobalStyle />
+        <body>
+          <GovUK.TopNav
+            bgColor="#000033"
+            serviceTitle={
+              <GovUK.TopNav.Anchor as={Link} to="/">
+                CMS Renderer Frontend
+              </GovUK.TopNav.Anchor>
+            }
+            search={
+              <GovUK.SearchBox>
+                {SearchBox.Input && <SearchBox.Input placeholder="Search GOV.UK" />}
+                {SearchBox.Button && <SearchBox.Button />}
+              </GovUK.SearchBox>
+            }>
+            <GovUK.TopNav.NavLink as={Link} to="/">
+              Home
+            </GovUK.TopNav.NavLink>
+            <GovUK.TopNav.NavLink as={Link} to="/forms">
+              Forms
+            </GovUK.TopNav.NavLink>
+          </GovUK.TopNav>
+          <GovUK.Page.WidthContainer>
+            <GovUK.PhaseBanner level="alpha">
+              This part of GOV.UK is being rebuilt – <Link href="https://example.com">find out what that means</Link>
+            </GovUK.PhaseBanner>
+            <GovUK.Page.Main>{children}</GovUK.Page.Main>
+          </GovUK.Page.WidthContainer>
+          <div style={{ position: "fixed", bottom: 0, width: "100vw" }}>
+            <GovUK.Footer />
+          </div>
+        </body>
+      </StyledComponentsRegistry>
     </html>
   );
 }
