3186f5e7ed61b451a20c16dd1d76df65f0b8ccbf
Rav Sispal <rav.sispal@ravsispal.com>
2024-05-13 01:46:09 +0100

    Add GovUK React dependency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 "use client";
+import * as GovUK from "govuk-react";
+import { Link, SearchBox } from "govuk-react";
 
 export default function RootLayout({
   children,
@@ -7,8 +9,37 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GovUK.GlobalStyle />
       <body>
-        <main>{children}</main>
+        <GovUK.TopNav
+          bgColor="#000033"
+          serviceTitle={
+            <GovUK.TopNav.Anchor as={Link} to="/">
+              CMS Renderer Frontend
+            </GovUK.TopNav.Anchor>
+          }
+          search={
+            <GovUK.SearchBox>
+              {SearchBox.Input && <SearchBox.Input placeholder="Search GOV.UK" />}
+              {SearchBox.Button && <SearchBox.Button />}
+            </GovUK.SearchBox>
+          }>
+          <GovUK.TopNav.NavLink as={Link} to="/">
+            Home
+          </GovUK.TopNav.NavLink>
+          <GovUK.TopNav.NavLink as={Link} to="/forms">
+            Forms
+          </GovUK.TopNav.NavLink>
+        </GovUK.TopNav>
+        <GovUK.Page.WidthContainer>
+          <GovUK.PhaseBanner level="alpha">
+            This part of GOV.UK is being rebuilt – <Link href="https://example.com">find out what that means</Link>
+          </GovUK.PhaseBanner>
+          <GovUK.Page.Main>{children}</GovUK.Page.Main>
+        </GovUK.Page.WidthContainer>
+        <div style={{ position: "fixed", bottom: 0, width: "100vw" }}>
+          <GovUK.Footer />
+        </div>
       </body>
     </html>
   );
