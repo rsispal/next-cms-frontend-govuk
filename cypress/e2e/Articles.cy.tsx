d5e21744a06d37d974280e9d4b9612b3c9e26172
Rav Sispal <rav.sispal@ravsispal.com>
2024-05-13 17:41:18 +0100

    Add tests

---
+++ cypress/e2e/Articles.cy.tsx
@@ -0,0 +1,24 @@
+describe("<ArticlesPage />", () => {
+  beforeEach(() => {
+    cy.visit("localhost:3000/articles");
+  });
+  it("Loads the Articles page", () => {
+    cy.get('[data-testid="page-title"]').should("have.text", "Articles");
+  });
+
+  it("Loads the test articles", () => {
+    cy.get('[data-testid="article-card"]').should("have.length", 4);
+  });
+
+  it("checks the href and visits the URL of the second article card", () => {
+    cy.get('[data-testid="article-card"]')
+      .eq(1)
+      .find("a")
+      .should("have.attr", "href")
+      .then((href) => {
+        expect(href).to.equal("/articles/002");
+
+        cy.visit(href);
+      });
+  });
+});
--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "hd.wallpaperswide.com",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
