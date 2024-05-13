de3d5403a83e50be8c3af84147761470dbf1676c
Rav Sispal <rav.sispal@ravsispal.com>
2024-05-13 01:23:56 +0100

    Setup nextjs, jest and cypress

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ src/constants/index.tsx
@@ -0,0 +1,10 @@
+export const APP_NAME = "CMS Frontend";
+export const APP_NAME_SHORT = "CMS FE";
+
+export enum LogLevel {
+  NONE,
+  DEBUG,
+  WARN,
+  ERROR,
+  FULL,
+}
